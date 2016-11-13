9a9614c7c01484d23acbb69cc53962023fc5dd4f
Fernando Garcia Samblas <fernando.garcia@the-cocktail.com>
2016-11-13 19:54:18 +0100

    Until "Check for missing email" section

--- web/controllers/user_controller.ex
+++ web/controllers/user_controller.ex
@@ -2,9 +2,16 @@ defmodule MyApp.UserController do
   use MyApp.Web, :controller
 
   def create(conn, params) do
-    {:ok, user} = MyApp.Users.Invite.call(params)
+    attrs = %{email: params["email"]}
+    {:ok, user} = MyApp.Users.Invite.call(attrs)
+
+    {:ok, body} = %{
+      id: user.id,
+      email: user.email
+    }
+    |> Poison.encode
 
     conn
-    |> resp(201, "{}")
+    |> resp(201, body)
   end
 end
